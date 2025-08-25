02ae2655b95353ec31dcf42c16f6f5eb6947b44c
rickiey <ralkaider@gmail.com>
2025-08-25 22:16:32 +0800

    fix:cid

--- ipfsx/cmd/cid.go
+++ ipfsx/cmd/cid.go
@@ -2,38 +2,49 @@ package cmd
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/ipfs/go-cid"
-	"github.com/ipfs/go-merkledag"
-	"github.com/ipfs/go-unixfs"
-	"github.com/multiformats/go-multihash"
-	"github.com/rickiey/ipfs-x/utils"
+	mc "github.com/multiformats/go-multicodec"
+	mh "github.com/multiformats/go-multihash"
 	"github.com/urfave/cli/v2"
 )
 
 func calculateCID(c *cli.Context) error {
 	filePath := c.String("file")
-	return calculateCIDv1(filePath)
+	fcid, err := calculateCIDv1(filePath)
+	if err != nil {
+		fmt.Printf("计算 CID 失败: %s\n", err)
+		return err
+	}
+	fmt.Printf("文件 %s 的 CID v1 为: %s\n", filePath, fcid)
+	return nil
 }
 
-func calculateCIDv1(filePath string) error {
-	// Read file content
-	data, err := utils.ReadFile(filePath)
+func calculateCIDv1(filePath string) (string, error) {
+	// 读取文件内容
+	fileData, err := os.ReadFile(filePath)
 	if err != nil {
-		return fmt.Errorf("failed to read file: %v", err)
+		fmt.Printf("读取文件失败: %s\n", err)
+		return "", err
 	}
 
-	// Create a PBNode
-	pbNode := unixfs.FSNodeFromBytes(data)
-	dagNode := merkledag.NewRawNode(pbNode.Data())
-
-	// Calculate CID v1
-	buidler := cid.V1Builder{Codec: cid.DagProtobuf, MhType: multihash.SHA2_256}
-	cidV1, err := buidler.Sum(dagNode.RawData())
-	if err != nil {
-		return fmt.Errorf("failed to calculate CID: %v", err)
+	// 创建 CID v1 的前缀
+	pref := cid.Prefix{
+		Version:  1,              // CID v1
+		Codec:    uint64(mc.Raw), // 使用 Raw 编码（可根据需求改为 DagProtobuf 等）
+		MhType:   mh.SHA2_256,    // 使用 SHA2-256 哈希
+		MhLength: -1,             // 默认长度
 	}
 
-	fmt.Printf("CID v1: %s", cidV1.String())
-	return nil
+	// 计算文件的 CID
+	c, err := pref.Sum(fileData)
+	if err != nil {
+		fmt.Printf("生成 CID 失败: %s\n", err)
+		return "", err
+	}
+
+	// 输出 CID
+	fmt.Println("CID v1:", c.String())
+	return c.String(), nil
 }
