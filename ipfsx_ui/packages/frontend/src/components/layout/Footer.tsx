64c54a7b0889361615bb51fec5ea0c8800e3ee1a
rickiey <ralkaider@gmail.com>
2025-08-30 23:39:50 +0800

    init ui

---
+++ ipfsx_ui/packages/frontend/src/components/layout/Footer.tsx
@@ -0,0 +1,50 @@
+import { useCurrentAccount } from '@mysten/dapp-kit'
+import { Link } from '@radix-ui/themes'
+import Faucet from '@suiware/kit/Faucet'
+import { HeartIcon, SearchIcon } from 'lucide-react'
+import ThemeSwitcher from '~~/components/ThemeSwitcher'
+import {
+  CONTRACT_PACKAGE_VARIABLE_NAME,
+  EXPLORER_URL_VARIABLE_NAME,
+} from '~~/config/network'
+import { packageUrl } from '~~/helpers/network'
+import { notification } from '~~/helpers/notification'
+import useNetworkConfig from '~~/hooks/useNetworkConfig'
+
+const Footer = () => {
+  const { useNetworkVariables } = useNetworkConfig()
+  const networkVariables = useNetworkVariables()
+  const explorerUrl = networkVariables[EXPLORER_URL_VARIABLE_NAME]
+  const packageId = networkVariables[CONTRACT_PACKAGE_VARIABLE_NAME]
+  const currentAccount = useCurrentAccount()
+
+  return (
+    <footer className="flex w-full flex-col items-center justify-between gap-3 p-3 sm:flex-row sm:items-end">
+      <div className="flex flex-row gap-3 lg:w-1/3">
+        {currentAccount != null && (
+          <>
+            <Faucet
+              onError={notification.error}
+              onSuccess={notification.success}
+            />
+            <Link
+              href={packageUrl(explorerUrl, packageId)}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="flex flex-row items-center gap-1"
+              highContrast={true}
+            >
+              <SearchIcon className="h-4 w-4" />
+              <span>Block Explorer</span>
+            </Link>
+          </>
+        )}
+      </div>
+
+      <div className="flex flex-row justify-end lg:w-1/3">
+        <ThemeSwitcher />
+      </div>
+    </footer>
+  )
+}
+export default Footer
